8b8fabd9c0c0fe6b249ef5f70dd90cbfd6065bb1
caarlosdamian <dapecarlos@gmail.com>
2023-03-21 23:34:35 -0600

    feat(init app)

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/Header.module.scss'
+
+export const Header = () => {
+  return (
+    <nav className={styles.header}>
+        <div className={styles.container}>header</div>
+    </nav>
+  )
+}
