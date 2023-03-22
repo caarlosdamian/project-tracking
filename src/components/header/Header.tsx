c07cc125185fef17e6244771968c52437a548c75
caarlosdamian <dapecarlos@gmail.com>
2023-03-22 00:09:31 -0600

    feat(components): added new components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import styles from '../../styles/Header.module.scss'
+import Image from "next/image";
+import React from "react";
+import styles from "../../styles/Header.module.scss";
+import logo from "../../images/logo.svg";
+import hamburger from "../../images/icon-hamburger.svg";
 
 export const Header = () => {
   return (
     <nav className={styles.header}>
-        <div className={styles.container}>header</div>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Image alt="logo" src={logo} className={styles.img} />
+        </div>
+        <div className={styles.hamburguer}>
+          <Image alt="logo" src={hamburger} />
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
---
+++ src/components/shape/Shape.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../../styles/Shape.module.scss";
+
+export const Shape = () => {
+  return <div className={styles.shape}></div>;
+};
